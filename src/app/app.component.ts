0dbfb7cd964f33c2534be8d31123a1e91529b425
TEJAS <31161839+HiThereItsMeTejas@users.noreply.github.com>
2018-09-18 12:01:02 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+
+// app.component.ts
+
+import { Component , OnInit} from '@angular/core';
+import { BookService } from './book.service';
+import { Book } from './Book';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'app';
+  books: Book[];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this
+      .bookService
+      .getBooks()
+      .subscribe((data: Book[]) => {
+        this.books = data;
+    });
+  }
+}
